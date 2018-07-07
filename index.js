94c936051686fe175cb5c37236e07643a4978223
Joseph Parker <jparker010190@gmail.com>
2018-07-07 15:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (block){
+  let result
+  if (block > 34){
+    result = block - 42
+  }
+  else {
+    result = (block - 42) * -1
+  }
+  return result
+}
+function distanceFromHqInFeet(feet){
+  let result = distanceFromHqInBlocks(feet);
+  let joe = result * 264
+  return joe
+}
+function distanceTravelledInFeet(start, end){
+  let result
+  if (end > 28){
+    result = (end - start) * 264
+  }
+  else {result = (start - end )*264}
+  return result
+}
+function calculatesFarePrice(start, destination){
+  let result = distanceTravelledInFeet(start, destination);
+  if (result  < 400 ){
+    result = 0
+  }
+  else if (result > 2000 && result < 2500){
+    result = 25
+  }
+  else if (result > 2500){
+    result = "cannot travel that far"
+  }
+  else {
+    result = 0
+  }
+  return result
+}
