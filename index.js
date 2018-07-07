9ecd6801d51d5a423c1e8d9eb542a4bd068fc6de
Joseph Parker <jparker010190@gmail.com>
2018-07-07 16:06:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function distanceTravelledInFeet(start, end){
 function calculatesFarePrice(start, destination){
   let result = distanceTravelledInFeet(start, destination);
   if (result > 400 && result < 2000 ){
-    result = (result - 400) * 0.02
+    result = (distanceTravelledInFeet - 400) * 0.02
     console.log(result)
   }
   else if (result > 2000 && result < 2500){
